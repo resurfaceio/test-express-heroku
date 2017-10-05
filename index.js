b1ca06dd8fb699e024b1ce991cdc3d1181e4cecf
Rob Dickinson <robfromboulder@gmail.com>
2017-10-04 22:08:53 -0400

    Add post handler

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index', function (err, html1) {
+        response.status(200).send(html1);
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.post('/', function (request, response) {
+    let html2 = '<html><p>Posted to Express:</p>';
+    const body = request.body;
+    for (const key in body) {
+        if (body.hasOwnProperty(key)) {
+            html2 += `<p>${key}:${body[key]}</p>`;
+        }
+    }
+    html2 += '</html>\n';
+    response.status(200).send(html2);
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
